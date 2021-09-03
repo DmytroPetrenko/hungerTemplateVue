836be1cadd2323cc57cf9b3d6a4fdbc99fd9d4ed
DmytroPetrenko <44116687+DmytroPetrenko@users.noreply.github.com>
2021-09-03 09:24:13 +0300

    init

--- src/main.js
+++ src/main.js
@@ -1,12 +1,22 @@
-import Vue from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
+import Vue from "vue"
+import App from "./App.vue"
+import router from "./router"
+import store from "./store"
 
-Vue.config.productionTip = false;
+import { library } from "@fortawesome/fontawesome-svg-core"
+import { faUserSecret } from "@fortawesome/free-solid-svg-icons"
+import { faFontAwesome } from "@fortawesome/free-brands-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome"
+
+library.add(faUserSecret)
+library.add(faFontAwesome)
+
+Vue.component("font-awesome-icon", FontAwesomeIcon)
+
+Vue.config.productionTip = false
 
 new Vue({
-  router,
-  store,
-  render: (h) => h(App),
-}).$mount("#app");
+	router,
+	store,
+	render: (h) => h(App),
+}).$mount("#app")
